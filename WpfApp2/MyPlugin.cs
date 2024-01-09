4fa680a70acf00b61105e23ff4dde47d23ee6bb5
Kazuki Ota <kaota@microsoft.com>
2024-01-09 10:21:21 +0900

    add log

--- WpfApp2/MyPlugin.cs
+++ WpfApp2/MyPlugin.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.SemanticKernel;
 using System.ComponentModel;
+using System.Diagnostics;
 
 namespace WpfApp2;
 public class MyPlugin
@@ -8,6 +9,8 @@ public class MyPlugin
     [Description("Sleep")]
     public async Task SleepAsync([Description("The number of seconds to sleep.")]int seconds)
     {
+        Debug.WriteLine($"SleepAsync(seconds: {seconds}) started.");
         await Task.Delay(seconds * 1000).ConfigureAwait(false);
+        Debug.WriteLine($"SleepAsync(seconds: {seconds}) finished.");
     }
 }
